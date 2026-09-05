0e7c543cc319e8d4133c25bd57c62eeefe5aa182
Asis Bethe <orangehermit-777@yahoo.co.jp>
2026-09-05 16:44:39 +0900

    wip

--- src/config/guns.js
+++ src/config/guns.js
@@ -1,7 +1,9 @@
 export const ACTIVE_GUN = Object.freeze({
   id: 'colt_python',
   displayName: 'Colt Python',
-  modelUrl: './assets/models/colt_python_357.glb',
+  // 設定モジュール自身を基準にすることで、tests/ 配下の検証ページからも
+  // 同じアセットを参照できる。
+  modelUrl: new URL('../../assets/models/colt_python_357.glb', import.meta.url).href,
   // 一時的に内部識別名を "bolt_byson" に変えたことがあるため、その名義で
   // 保存された較正値があれば現在のIDへ統合する。
   legacyIds: ['bolt_byson'],
