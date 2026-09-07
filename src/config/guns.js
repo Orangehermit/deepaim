5183b3fb79e6f595f4201d9bf518571d2be3bf54
Asis Bethe <orangehermit-777@yahoo.co.jp>
2026-09-07 20:30:03 +0900

    wip

--- src/config/guns.js
+++ src/config/guns.js
@@ -9,7 +9,7 @@ export const GUNS = Object.freeze({
     capacity: 16,
     ammoDisplayOffset: Object.freeze({ x: 0, y: 0.055, z: 0.10 }),
     selectionDisplayTransform: Object.freeze({
-      position: Object.freeze({ x: 0.05, y: -0.087, z: 0.4 }),
+      position: Object.freeze({ x: 0.05, y: -0.086, z: 0.4 }),
       rotation: Object.freeze({
         x: 0,
         y: 0, // Gun_Rootのローカル-Z、すなわちTraining Room（ワールド-Z）側へ
